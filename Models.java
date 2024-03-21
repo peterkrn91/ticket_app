5a66c7748a5f16624df168b5495164386e55fb55
peter <peter_krn@hotmail.com>
2024-03-21 16:03:31 +0700

    added class Ticket

--- Models.java
+++ Models.java
@@ -33,3 +33,42 @@ class User {
         return status;
     }
 }
+
+class Ticket {
+    private String id;
+    private String description;
+    private String status;
+
+    public Ticket() {
+    }
+
+    public Ticket(String id, String description, String status) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
